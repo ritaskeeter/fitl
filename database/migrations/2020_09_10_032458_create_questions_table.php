96d8c8784e2fdf781407239b7a756c20deb02fde
Sona Senthil <sonaskumaran@gmail.com>
2020-09-10 09:12:15 +0530

    Added a questions database migration

---
+++ database/migrations/2020_09_10_032458_create_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            //Custom code
+            $table->string('title'); //string maps to VARCHAR
+            $table->text('description');
+            $table->text('code')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('questions');
+    }
+}
